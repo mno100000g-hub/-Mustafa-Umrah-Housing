428348f44f3956bf8da196981e2e326e8b1be2c5
mno100000g-hub <mno100000g@gmail.com>
2026-02-16 16:14:42 +0200

    Create Navbar.tsx

---
+++ src/components/src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+export default function Navbar() {
+  return (
+    <div style={{
+      width: "100%",
+      padding: "15px",
+      background: "#0b5d3b",
+      color: "white",
+      textAlign: "center",
+      fontWeight: "bold",
+      fontSize: "20px"
+    }}>
+      القصل للعمرة والزيارة
+    </div>
+  );
+}
